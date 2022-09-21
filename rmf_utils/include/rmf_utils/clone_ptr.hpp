5845fbf3e4d52e58dbc42c6fd4812f3c3567f8a2
Esteban Martinena <orensbruli@gmail.com>
2022-09-21 16:10:36 +0200

    Minor uncrustify fix
    Signed-off-by: Esteban Martinena <orensbruli@gmail.com>

--- rmf_utils/include/rmf_utils/clone_ptr.hpp
+++ rmf_utils/include/rmf_utils/clone_ptr.hpp
@@ -39,7 +39,7 @@ class clone_ptr;
 
 //==============================================================================
 template<typename T, typename... Args>
-clone_ptr<T> make_clone(Args&&... args);
+clone_ptr<T> make_clone(Args&& ... args);
 
 //==============================================================================
 template<typename T>
@@ -194,7 +194,7 @@ private:
 
 //==============================================================================
 template<typename T, typename... Args>
-clone_ptr<T> make_clone(Args&&... args)
+clone_ptr<T> make_clone(Args&& ... args)
 {
   return clone_ptr<T>(new T(std::forward<Args>(args)...));
 }
